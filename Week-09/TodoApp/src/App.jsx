9bc32badd7e96d96d7a06e92b8176e9c566ed163
muskanqed <muskanqed@gmail.com>
2024-12-28 19:08:58 +0530

    Done with TodoApp

--- Week-09/TodoApp/src/App.jsx
+++ Week-09/TodoApp/src/App.jsx
@@ -4,11 +4,33 @@ import Todo from "./components/Todo";
 import Form from "./components/Form";
 import FilterButton from "./components/FilterButton";
 
+const FILTER_MAP = {
+  All: () => true,
+  Active: (task) => !task.completed,
+  Completed: (task) => task.completed,
+};
+
+const FILTER_NAMES = Object.keys(FILTER_MAP);
+
 function App(props) {
   const [tasks, setTasks] = useState(props.tasks);
+  const [filter, setFilter] = useState("All");
+
+  function editTask(id, newName) {
+    const editedTaskList = tasks.map((task) => {
+      // if this task has the same ID as the edited task
+      if (id === task.id) {
+        // Copy the task and update its name
+        return { ...task, name: newName };
+      }
+      // Return the original task if it's not the edited task
+      return task;
+    });
+    setTasks(editedTaskList);
+  }
 
   function deleteTask(id) {
-    const remainingTasks = tasks.filter((task) => id !== task.id)
+    const remainingTasks = tasks.filter((task) => id !== task.id);
     setTasks(remainingTasks);
     // The code id !== task.id is part of the filter method, which is used to create a new array by including only the items that satisfy the given condition.
     // If id !== task.id evaluates to true, the task will be included in the new array (remainingTasks).
@@ -18,21 +40,33 @@ function App(props) {
   function toggleTaskCompleted(id) {
     const updatedTasks = tasks.map((task) => {
       if (id === task.id) {
-        return { ...task, completed: !task.completed }
+        return { ...task, completed: !task.completed };
       }
       return task;
     });
-    setTasks(updatedTasks)
+    setTasks(updatedTasks);
   }
 
-  const taskList = tasks?.map((task) => (
-    <Todo
-      id={task.id}
-      name={task.name}
-      completed={task.completed}
-      key={task.id}
-      toggleTaskCompleted={toggleTaskCompleted}
-      deleteTask={deleteTask}
+  const taskList = tasks
+    .filter(FILTER_MAP[filter])
+    .map((task) => (
+      <Todo
+        id={task.id}
+        name={task.name}
+        completed={task.completed}
+        key={task.id}
+        toggleTaskCompleted={toggleTaskCompleted}
+        deleteTask={deleteTask}
+        editTask={editTask}
+      />
+    ));
+
+  const filterList = FILTER_NAMES.map((name) => (
+    <FilterButton
+      key={name}
+      name={name}
+      isPressed={name === filter}
+      setFilter={setFilter}
     />
   ));
 
@@ -41,25 +75,24 @@ function App(props) {
 
   function addTask(name) {
     const newTask = { id: `todo-${nanoid()}`, name, completed: false };
-    setTasks([...tasks, newTask])
-
+    setTasks([...tasks, newTask]);
   }
 
   return (
     <div className="todoapp stack-large">
       <h1>TodoMatic</h1>
       <Form addTask={addTask} />
-      <FilterButton />
+      {filterList}
       <h2 id="list-heading">{headingText}</h2>
       <ul
         role="list"
         className="todo-list stack-large stack-exception"
-        aria-labelledby="list-heading">
+        aria-labelledby="list-heading"
+      >
         {taskList}
       </ul>
-
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
--- Week-09/TodoApp/src/components/FilterButton.jsx
+++ Week-09/TodoApp/src/components/FilterButton.jsx
@@ -1,15 +1,18 @@
-function FilterButton() {
-    return (
-        <>
-            <div className="filters btn-group stack-exception">
-                <button type="button" className="btn toggle-btn" aria-pressed="true">
-                    <span className="visually-hidden">Show </span>
-                    <span>all</span>
-                    <span className="visually-hidden"> tasks</span>
-                </button>
-            </div>
-        </>
-    )
+function FilterButton(props) {
+  return (
+    <div className="filters btn-group stack-exception">
+      <button
+        type="button"
+        className="btn toggle-btn"
+        aria-pressed={props.isPressed}
+        onClick={() => props.setFilter(props.name)}
+      >
+        <span className="visually-hidden">Show </span>
+        <span>{props.name}</span>
+        <span className="visually-hidden"> tasks</span>
+      </button>
+    </div>
+  );
 }
 
 export default FilterButton;
