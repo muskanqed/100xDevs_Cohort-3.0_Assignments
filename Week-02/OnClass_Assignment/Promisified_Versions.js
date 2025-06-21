53c141bc4fa1ebb7c7d6c751775027c4ea9f0595
muskanqed <muskanqed@gmail.com>
2025-06-21 03:56:26 -0400

    Started with week-02 on class assignment

---
+++ Week-02/OnClass_Assignment/Promisified_Versions.js
@@ -0,0 +1,45 @@
+const fs = require("fs");
+
+// Promisified version of setTimeout
+
+function setTimeoutPromisified(ms) {
+  return new Promise((resolve) => {
+    setTimeout(resolve, ms);
+  });
+}
+
+setTimeoutPromisified(3000).then((callback) => {
+  console.log("Promisified version of setTimeout");
+});
+
+// Promisified version of fetch
+function fetchPromisified(url) {
+  return new Promise((resolve, reject) => {
+    fetch(url)
+      .then((data) => resolve(data))
+      .catch((error) => reject(error));
+  });
+}
+
+promisifiedFetch("https://jsonplaceholder.typicode.com/todos/1")
+  .then((response) => response.json())
+  .then((data) => console.log(data))
+  .catch((error) => console.log(error));
+
+// Promisified version of fs.readFile
+
+function readFilePromisified(fileName) {
+  return new Promise((resolve, reject) => {
+    fs.readFile(fileName, "utf-8", (err, data) => {
+      if (err) {
+        reject(err);
+      } else {
+        resolve(data);
+      }
+    });
+  });
+}
+
+readFilePromisified("a.txt").then((data) => {
+  console.log(data);
+});
