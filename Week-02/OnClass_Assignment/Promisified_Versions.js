acc2e9437dbed8820641e159db85652e24ee8d03
muskanqed <muskanqed@gmail.com>
2025-06-27 06:41:18 -0400

    Week-02 hard question - 01

--- Week-02/OnClass_Assignment/Promisified_Versions.js
+++ Week-02/OnClass_Assignment/Promisified_Versions.js
@@ -21,7 +21,7 @@ function fetchPromisified(url) {
   });
 }
 
-promisifiedFetch("https://jsonplaceholder.typicode.com/todos/1")
+fetchPromisified("https://jsonplaceholder.typicode.com/todos/1")
   .then((response) => response.json())
   .then((data) => console.log(data))
   .catch((error) => console.log(error));
